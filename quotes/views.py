8983e217612829e5a8106a3b2f09943ddf931ef3
Galina Troshkina <maystar2009@gmail.com>
2021-02-24 23:42:13 +0300

    initial commit

---
+++ quotes/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+
+def home(request):
+    import requests
+    import json
+
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
+
+        api_request = requests.get(
+            "https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_290e67cf8b9742558f43e727ec700fdd")
+
+        try:
+            api = json.loads(api_request.content)
+
+        except Exception as e:
+            api = 'Error...'
+
+        return render(request, 'home.html', {'api': api})
+
+    else:
+        return render(request, 'home.html', {'ticker': 'Enter a Ticker Symbol Above'})
+
+
+def about(request):
+    return render(request, 'about.html', {})
